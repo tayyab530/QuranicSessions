0fd8e1ad5d8c066332a0f0f9705e9908544bce48
tayyab530 <mmtayyab530@gmail.com>
2021-03-05 11:27:32 +0500

    Icon and background Tabs Color

--- lib/src/screens/main_screen.dart
+++ lib/src/screens/main_screen.dart
@@ -21,21 +21,25 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_selectedTab == 0 ? 'Available Sessions': 'Your Session'),
+        title: Text(_selectedTab == 0 ? 'Available Sessions' : 'Your Session'),
       ),
       body: _tabsScreens[_selectedTab],
       bottomNavigationBar: BottomNavigationBar(
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.event_available_rounded),label: 'Live Sessions' ),
-          BottomNavigationBarItem(icon: Icon(Icons.favorite_rounded),label: 'Your Sessions'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.event_available_rounded),
+              label: 'Live Sessions'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_rounded), label: 'Your Sessions'),
         ],
         onTap: (index) {
-            setState(() {
-              _selectedTab = index;
-            });
-          
+          setState(() {
+            _selectedTab = index;
+          });
         },
         currentIndex: _selectedTab,
+        backgroundColor: Theme.of(context).accentColor,
+        selectedItemColor: Colors.blue[400],
       ),
     );
   }
